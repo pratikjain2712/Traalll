94aeb183ec3f6ae155b57fbbc29ad0b1b8083ee3
Pratik <p.jain@interproductec.com>
2019-07-02 18:16:27 +0530

    Added the sidebar drawer and the logo section

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker({   
+      autoClose:true,
+      showClearBtn:false
+    });
+  });
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+       
+
+  
